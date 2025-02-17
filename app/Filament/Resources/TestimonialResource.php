7538bf261f45a1b536bc003959dbbee57d74487b
adrianmulyawan <adrianmulyawan666@gmail.com>
2025-02-17 19:40:19 +0700

    add rule in model and crud

---
+++ app/Filament/Resources/TestimonialResource.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TestimonialResource\Pages;
+use App\Filament\Resources\TestimonialResource\RelationManagers;
+use App\Models\Testimonial;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TestimonialResource extends Resource
+{
+    protected static ?string $model = Testimonial::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-user';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                FileUpload::make('photo')
+                    ->label('Photo')
+                    ->image()
+                    ->directory('testimonials')
+                    ->columnSpan(2),
+                Select::make('boarding_house_id')
+                    ->label('Boarding House')
+                    ->relationship('boardingHouse', 'name')
+                    ->columnSpan(2),
+                TextInput::make('name')
+                    ->label('Nama Pengguna')
+                    ->columnSpan(2),
+                TextInput::make('rating')
+                    ->label('Rating')
+                    ->numeric()
+                    ->minValue(1)
+                    ->maxValue(5)
+                    ->columnSpan(2),
+                RichEditor::make('content')
+                    ->label('Testimoni Pengguna')
+                    ->columnSpan(2),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                ImageColumn::make('photo')
+                    ->label('Photo'),
+                TextColumn::make('name')
+                    ->label('Nama')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('boardingHouse.name')
+                    ->label('Penginapan')
+                    ->searchable(),
+                TextColumn::make('rating')
+                    ->label('Nilai')
+                    ->searchable(),
+            ])
+            ->defaultSort('id', 'DESC')
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTestimonials::route('/'),
+            'create' => Pages\CreateTestimonial::route('/create'),
+            'edit' => Pages\EditTestimonial::route('/{record}/edit'),
+        ];
+    }
+}
