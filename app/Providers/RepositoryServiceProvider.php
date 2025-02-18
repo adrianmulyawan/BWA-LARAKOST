1441024b146d05b620468e5f8e4e991f8bd2802f
adrianmulyawan <adrianmulyawan666@gmail.com>
2025-02-18 14:58:40 +0700

    Register repository

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,16 +2,24 @@
 
 namespace App\Providers;
 
+use App\Interfaces\BoardingHouseRepositoryInterface;
+use App\Interfaces\CategoryRepositoryInterfaces;
+use App\Interfaces\CityRepositoryInterface;
+use App\Repositories\BoardingHouseRepository;
+use App\Repositories\CategoryRepository;
+use App\Repositories\CityRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
     /**
-     * Register services.
+     * Register services. (Daftarkan servicenya)
      */
     public function register(): void
     {
-        //
+        $this->app->bind(CityRepositoryInterface::class, CityRepository::class);
+        $this->app->bind(CategoryRepositoryInterfaces::class, CategoryRepository::class);
+        $this->app->bind(BoardingHouseRepositoryInterface::class, BoardingHouseRepository::class);
     }
 
     /**
