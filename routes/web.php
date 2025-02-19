5e75ce4a11df43cdb51b5e4993b7e4f125dede55
adrianmulyawan <adrianmulyawan666@gmail.com>
2025-02-19 16:59:06 +0700

    add new routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\BoardingHouseController;
 use App\Http\Controllers\BookingController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,6 @@ Route::get('/find-boarding-house', [BoardingHouseController::class, 'find'])
     ->name('boarding-house.find');
 Route::get('/result-boarding-house', [BoardingHouseController::class, 'findResult'])
     ->name('boarding-house.result');
+
+Route::get('/categories/{slug:name}', [CategoryController::class, 'show'])
+    ->name('category.show');
