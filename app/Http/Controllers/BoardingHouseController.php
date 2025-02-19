ea9d0f847a62790fbdf61c9293d42956a953c05a
adrianmulyawan <adrianmulyawan666@gmail.com>
2025-02-19 14:20:44 +0700

    create new controller : Boarding House Controller

---
+++ app/Http/Controllers/BoardingHouseController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Interfaces\BoardingHouseRepositoryInterface;
+use App\Interfaces\CategoryRepositoryInterface;
+use App\Interfaces\CityRepositoryInterface;
+use Illuminate\Http\Request;
+
+class BoardingHouseController extends Controller
+{
+    private CityRepositoryInterface $cityRepository;
+    private CategoryRepositoryInterface $categoryRepository;
+    private BoardingHouseRepositoryInterface $boadrdingHouseRepository;
+
+    public function __construct(
+        CityRepositoryInterface $cityRepository,
+        CategoryRepositoryInterface $categoryRepository,
+        BoardingHouseRepositoryInterface $boardingHouseRepository
+    ) {
+        $this->cityRepository = $cityRepository;
+        $this->categoryRepository = $categoryRepository;
+        $this->boadrdingHouseRepository = $boardingHouseRepository;
+    }
+
+    public function find()
+    {
+        $cities = $this->cityRepository->getAllCities();
+        $categories = $this->categoryRepository->getAllCategories();
+
+        return view('pages.boarding_house.find-boarding-house', compact(
+            'cities',
+            'categories'
+        ));
+    }
+}
