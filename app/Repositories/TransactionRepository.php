d97b764f8539eb2feed3a14e3ada08d3dbfdff34
adrianmulyawan <adrianmulyawan666@gmail.com>
2025-02-21 17:29:05 +0700

    add method to save and callculate transaction

--- app/Repositories/TransactionRepository.php
+++ app/Repositories/TransactionRepository.php
@@ -3,6 +3,8 @@
 namespace App\Repositories;
 
 use App\Interfaces\TransactionRepositoryInterface;
+use App\Models\Room;
+use App\Models\Transaction;
 
 class TransactionRepository implements TransactionRepositoryInterface
 {
@@ -20,5 +22,59 @@ class TransactionRepository implements TransactionRepositoryInterface
         }
 
         session()->put('transaction', $transaction);
+    }
+
+    public function saveTransaction($data)
+    {
+        $room = Room::find($data['room']);
+        $data = $this->prepareTransactionData($data, $room);
+        // dd($data);
+
+        $transaction = Transaction::create([
+            'code' => $data["code"],
+            'boarding_house_id' => $data["boarding_house_id"],
+            'room_id' => $data["room"],
+            'name' => $data["name"],
+            'email' => $data["email"],
+            'phone_number' => $data["phone_number"],
+            'payment_method' => $data["payment_method"],
+            'payment_status' => $data["payment_status"],
+            'start_date' => $data["start_date"],
+            'duration' => $data["duration"],
+            'total_amount' => $data["total_amount"],
+            'transaction_date' => $data["transaction_date"],
+        ]);
+        session()->forget('transaction');
+        return $transaction;
+    }
+
+    private function prepareTransactionData($data, $room)
+    {
+        $data['code'] = $this->generateTransactionCode();
+        $data['payment_status'] = 'pending';
+        $data['transaction_date'] = now();
+
+        $total = $this->calculateTotalAmount($room->price_per_month, $data['duration']);
+        $data['total_amount'] = $this->calculatePaymentAmount($total, $data['payment_method']);
+
+        return $data;
+    }
+
+    private function generateTransactionCode()
+    {
+        return 'NGKBWA' . rand(100000, 999999);
+    }
+
+    private function calculateTotalAmount($pricePerMonth, $duration)
+    {
+        $subtotal = $pricePerMonth * $duration;
+        $tax = $subtotal * 0.11;
+        $insurance = $subtotal * 0.01;
+        return $subtotal + $tax + $insurance;
+    }
+
+    private function calculatePaymentAmount($total, $paymentMethod)
+    {
+        return $paymentMethod === 'full_payment' ? $total : $total * 0.3;
     }
 }
